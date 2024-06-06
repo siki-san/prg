2de6e5a2eb2ae759a4870f54c070bff16dd194b1
siki-san <136573802+siki-san@users.noreply.github.com>
2024-06-06 23:30:42 +0900

    じゃんけん

--- 00/jyan.c
+++ 00/jyan.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 int main (void){
-    int z;
+    int z;/*選ばれたグーかチョキかパーが入るところ*/
+    int y;/*コンピュータの手(相手のて)*/
+
+    srand(time(NULL));  /*乱数の種を初期化*/
 
     puts("じゃんけんタイム");
-    puts("グーなら0,チョキなら1,パーなら2を押してください。");
+    puts("グーなら1,チョキなら2,パーなら3を押してください。");
     puts("それでは始めます。");
     printf("さいしょはグーじゃんけんポン:");
-    scanf("%d",&z);
+    scanf("%d",&z);   
 
-    if(z == 0){
+    if(z == 1){
         printf("あなたが出したのはグーです。");
-    }else if(z == 1){
-        printf("あなたが出したのはチョキです。");
+        y = rand()%3+1;
+        if(y == 1){
+            printf("相手が出したのはグーです。");
+            printf("あいこです。");
+        }else if(y == 2){
+            printf("相手が出したのはチョキです。");
+            printf("あなたの勝ちです。");
+        }else if(y ==3){
+            printf("相手が出したのはパーです。");
+            printf("あなたの負けです。");
+        }
     }else if(z == 2){
+        printf("あなたが出したのはチョキです。");
+        y = rand()%3+1;
+        if(y == 1){
+            printf("相手が出したのはグーです。");
+            printf("あなたの負けです");
+        }else if(y == 2){
+            printf("相手が出したのはチョキです。");
+            printf("あいこです。");
+        }else if(y ==3){
+            printf("相手が出したのはパーです。");
+            printf("あなたの勝ちです。");
+        }
+    }else if(z == 3){
         printf("あなたが出したのはパーです。");
+        y = rand()%3+1;
+        if(y == 1){
+            printf("相手が出したのはグーです。");
+            printf("あなたの勝ちです。");
+        }else if(y == 2){
+            printf("相手が出したのはチョキです。");
+            printf("あなたの負けです。");
+        }else if(y ==3){
+            printf("相手が出したのはパーです。");
+            printf("あいこです。");
+        }
     }else{
-        printf("グー:0,チョキ:1,パー:2のどれかで出してください。\n");
+        printf("グー:1,チョキ:2,パー:3のどれかで出してください。\n");
     }
+
 
 return 0;
 
